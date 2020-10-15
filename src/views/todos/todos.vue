4877e2de00a40a3381989658f9a69b0bbaa0f931
Deangle <602020050@qq.com>
2020-10-15 14:56:23 +0800

    feat: 实现分类列表、清除已办列表，待办数量展示功能

--- src/views/todos/todos.vue
+++ src/views/todos/todos.vue
@@ -8,28 +8,24 @@
             id="input"
             placeholder="What needs to be done?"
             @keyup.enter="addTodo"
-            v-model:value="todo"
+            v-model:value="state.todo"
           ></a-input>
         </a-layout-header>
         <a-layout-content v-if="state.todoList.length">
           <a-checkbox
             id="check-all"
             @change="onCheckAllChange"
-            v-model:checked="checkAll"
+            v-model:checked="state.checkAll"
             v-if="state.todoList.length"
           >
             <span></span>
           </a-checkbox>
-          <a-card
-            v-for="(item, index) in state.todoList"
-            :key="index"
-            @dblclick="edit(item)"
-          >
+          <a-card v-for="(item, index) in fileterTodoList" :key="index">
             <a-checkbox
               v-model:checked="item.done"
               @change="onChange(index)"
             ></a-checkbox>
-            <div v-if="!item.editing">
+            <div v-if="!item.editing" @dblclick="edit(item)">
               <span class="time" :class="{ completed: item.done }">
                 {{ item.time }}
               </span>
@@ -48,44 +44,86 @@
             <span class="destroy" @click="destroy(item)"></span>
           </a-card>
         </a-layout-content>
+        <a-layout-footer v-if="state.todoList.length">
+          <div class="left">
+            <span>{{ todos.length }} item left</span>
+          </div>
+          <div class="middle">
+            <ul>
+              <li
+                :class="{ selected: state.selected === 'all' }"
+                @click="selectList('all')"
+              >
+                All
+              </li>
+              <li
+                :class="{ selected: state.selected === 'active' }"
+                @click="selectList('active')"
+              >
+                Active
+              </li>
+              <li
+                :class="{ selected: state.selected === 'completed' }"
+                @click="selectList('completed')"
+              >
+                Completed
+              </li>
+            </ul>
+          </div>
+          <div class="right">
+            <span v-if="dones.length" @click="clearCompleted"
+              >Clear completed</span
+            >
+          </div>
+        </a-layout-footer>
       </a-layout>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, watch, onMounted, nextTick } from 'vue';
+import { reactive, watch, onMounted, nextTick, computed } from 'vue';
 import './todos.css';
 
 export default {
   setup() {
-    let checkAll = ref(false);
-    let todo = ref('');
-    let edting = ref(false);
     const time = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
     const state = reactive({
       todoList: JSON.parse(localStorage.getItem('todoList')) || [],
+      todo: '',
+      checkAll: false,
+      selected: 'all',
     });
+
+    // todoList 分类
+    const todoList = {
+      all(todoList) {
+        return todoList;
+      },
+      active(todoList) {
+        return todoList.filter((item) => !item.done);
+      },
+      completed(todoList) {
+        return todoList.filter((item) => item.done);
+      },
+    };
 
     // 添加
     const addTodo = (e) => {
-      if (!todo.value) return;
+      if (!state.todo) return;
       state.todoList.push({
         id: +new Date(),
         done: false,
         time: time,
-        content: todo.value,
+        content: state.todo,
       });
-      localStorage.setItem('todoList', JSON.stringify(state.todoList));
-
-      todo.value = '';
+      state.todo = '';
     };
 
     // 全选
     const onCheckAllChange = () => {
       state.todoList.forEach((item) => {
-        item.done = !item.done;
-        checkAll.value = !checkAll.value;
+        item.done = state.checkAll;
       });
     };
 
@@ -98,7 +136,6 @@ export default {
     // 删除
     const destroy = (item) => {
       state.todoList.splice(state.todoList.indexOf(item), 1);
-      localStorage.setItem('todoList', JSON.stringify(state.todoList));
     };
 
     // 编辑
@@ -111,6 +148,31 @@ export default {
       item.editing = false;
     };
 
+    // 计算待办列表
+    const todos = computed(() => {
+      return state.todoList.filter((item) => !item.done);
+    });
+
+    // 计算已办列表
+    const dones = computed(() => {
+      return state.todoList.filter((item) => item.done);
+    });
+
+    // 清空已办列表
+    const clearCompleted = () => {
+      state.todoList = state.todoList.filter((item) => !item.done);
+    };
+
+    // 选择分类：全部、待办、已办
+    const selectList = (type) => {
+      state.selected = type;
+    };
+
+    // 通过计算属性，计算 todoList 列表在不同分类下数据展示的不同
+    const fileterTodoList = computed(() => {
+      return todoList[state.selected](state.todoList);
+    });
+
     // 监听 todoList，单选时控制全选是否勾选
     watch(state.todoList, (list) => {
       let count = 0;
@@ -118,19 +180,25 @@ export default {
         if (item.done) count++;
       });
 
-      checkAll.value = count === list.length;
+      state.checkAll = count === list.length;
+      // 只要 todoList 有更改，就存入 localStorage
+      localStorage.setItem('todoList', JSON.stringify(state.todoList));
     });
 
     return {
       state,
-      todo,
       addTodo,
-      checkAll,
       onCheckAllChange,
       onChange,
       destroy,
       edit,
       editSure,
+      todos,
+      dones,
+      clearCompleted,
+      selectList,
+      todoList,
+      fileterTodoList,
     };
   },
   directives: {
